fix: minor fixes to links jacobian

genesis/engine/entities/rigid_entity/rigid_equality.py
@@ -53,11 +53,11 @@ class RigidEquality(RBC):
 
     @gs.assert_built
     def link1_jac(self, envs_idx=None) -> np.ndarray:
-        return self._solver.get_links_jac([self._link1id], envs_idx)
+        return self._solver.get_links_jac([self._link1id], [envs_idx], dof_start=self._entity.dof_start)
 
     @gs.assert_built
     def link2_jac(self, envs_idx=None) -> np.ndarray:
-        return self._solver.get_links_jac([self._link1id], envs_idx)
+        return self._solver.get_links_jac([self._link1id], [envs_idx], dof_start=self._entity.dof_start)
 
     def dim(self) -> int:
         if self._type == EQ_TYPE.CONNECT: