7c2b01ce6483ec8821eba13a90e6a139d8d46af8
Ivan Domrachev <domrachev03@mail.ru>
2025-01-04 18:06:24 +0300

    example: setup to match jacobians in mjx and genesis

--- examples/closed-kin-chain/robotiq_gripper_mjx.py
+++ examples/closed-kin-chain/robotiq_gripper_mjx.py
@@ -1,6 +1,14 @@
 import numpy as np
 import mujoco as mj
 from mujoco import viewer
+import argparse
+import os
+import pickle as pkl
+
+import numpy as np
+
+import genesis as gs
+import genesis.engine.entities as gsen
 
 
 def main():
@@ -8,12 +16,42 @@ def main():
     mj_model = mj.MjModel.from_xml_path(gripper_path)
 
     mj_data = mj.MjData(mj_model)
-    # mj_viewer = viewer.launch_passive(
-    #     mj_model,
-    #     mj_data,
-    #     show_left_ui=False,
-    #     show_right_ui=False,
-    # )
+
+    gs.init(backend=gs.gpu)
+
+    ########################## create a scene ##########################
+    viewer_options = gs.options.ViewerOptions(
+        camera_pos=(2.5, 0.0, 1.5),
+        camera_lookat=(0.0, 0.0, 0.5),
+        camera_fov=30,
+        max_FPS=60,
+    )
+
+    scene = gs.Scene(
+        sim_options=gs.options.SimOptions(
+            dt=0.001,
+        ),
+        viewer_options=viewer_options,
+        show_viewer=False,
+        show_FPS=False,
+    )
+
+    ########################## entities ##########################
+    scene.add_entity(
+        gs.morphs.Plane(),
+    )
+    scene.add_entity(
+        morph=gs.morphs.MJCF(
+            file="mujoco_menagerie/robotiq_2f85_v4/2f85.xml",
+            pos=(0.0, 0, 0.02),
+        ),
+    )
+
+    ########################## build ##########################
+    scene.build()
+    print("-" * 20, "Jacobians", "-" * 20)
+    print("mjx:")
+
     jacp1, jacr1 = np.zeros((3, mj_model.nv)), np.zeros((3, mj_model.nv))
     jacp2, jacr2 = np.zeros((3, mj_model.nv)), np.zeros((3, mj_model.nv))
     for i in range(1):
@@ -25,11 +63,14 @@ def main():
         )
         mj.mj_step(mj_model, mj_data)
         mj.mj_jacBody(mj_model, mj_data, jacp1, jacr1, id1)
-        with np.printoptions(suppress=True, linewidth=500):
-            print(jacp1)
         mj.mj_jacBody(mj_model, mj_data, jacp2, jacr2, id2)
         with np.printoptions(suppress=True, linewidth=500):
+            print("b1")
+            print(jacp1)
+            print("b2")
             print(jacp2)
+    print("genesis:")
+    scene.step()
 
 
 if __name__ == "__main__":
