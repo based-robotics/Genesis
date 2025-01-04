9ae8abe9e53e98ff267e7ec8a00785414a80ba4a
Ivan Domrachev <domrachev03@mail.ru>
2025-01-04 17:57:34 +0300

    example: add control input to robotiq example; update mjx example

--- examples/closed-kin-chain/robotiq_gripper.py
+++ examples/closed-kin-chain/robotiq_gripper.py
@@ -11,10 +11,11 @@ import genesis.engine.entities as gsen
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--vis", action="store_true", default=False)
+    parser.add_argument("-d", "--debug", action="store_true", default=False)
     args = parser.parse_args()
 
     ########################## init ##########################
-    gs.init(backend=gs.gpu)
+    gs.init(backend=gs.gpu, debug=args.debug)
 
     ########################## create a scene ##########################
     viewer_options = gs.options.ViewerOptions(
@@ -26,7 +27,7 @@ def main():
 
     scene = gs.Scene(
         sim_options=gs.options.SimOptions(
-            dt=0.01,
+            dt=0.001,
         ),
         viewer_options=viewer_options,
         show_viewer=args.vis,
@@ -50,10 +51,13 @@ def main():
     scene.build()
     for i in range(len(scene.rigid_solver.links)):
         print(f"Entity with id {scene.rigid_solver.links[i].idx}: {scene.rigid_solver.links[i].name}")
+
+    j1_id = gripper.get_joint("left_driver_joint").dof_start
+    j2_id = gripper.get_joint("right_driver_joint").dof_start
+
     for i in range(100000000000):
-        gripper.control_dofs_force(np.array([np.sin(i / 100), np.sin(i / 100)]), [2, 4])
+        gripper.control_dofs_force(np.array([-np.sin(i / 10) * 20, np.sin(i / 10) * 20]), [j1_id, j2_id])
         scene.step()
-        print(scene.rigid_solver.constraint_solver.aref)
 
 
 if __name__ == "__main__":
--- examples/closed-kin-chain/robotiq_gripper_mjx.py
+++ examples/closed-kin-chain/robotiq_gripper_mjx.py
@@ -1,3 +1,4 @@
+import numpy as np
 import mujoco as mj
 from mujoco import viewer
 
@@ -7,18 +8,28 @@ def main():
     mj_model = mj.MjModel.from_xml_path(gripper_path)
 
     mj_data = mj.MjData(mj_model)
-    mj_viewer = viewer.launch_passive(
-        mj_model,
-        mj_data,
-        show_left_ui=False,
-        show_right_ui=False,
-    )
-
-    for i in range(2):
+    # mj_viewer = viewer.launch_passive(
+    #     mj_model,
+    #     mj_data,
+    #     show_left_ui=False,
+    #     show_right_ui=False,
+    # )
+    jacp1, jacr1 = np.zeros((3, mj_model.nv)), np.zeros((3, mj_model.nv))
+    jacp2, jacr2 = np.zeros((3, mj_model.nv)), np.zeros((3, mj_model.nv))
+    for i in range(1):
+        id1, id2 = mj_model.eq_obj1id[0], mj_model.eq_obj2id[0]
+        print(id1, id2)
+        print(
+            mj.mj_id2name(mj_model, mj.mjtObj.mjOBJ_BODY, id1),
+            mj.mj_id2name(mj_model, mj.mjtObj.mjOBJ_BODY, id2),
+        )
         mj.mj_step(mj_model, mj_data)
-        print(mj_data.efc_aref)
-        print(mj_data.)
-        mj_viewer.sync()
+        mj.mj_jacBody(mj_model, mj_data, jacp1, jacr1, id1)
+        with np.printoptions(suppress=True, linewidth=500):
+            print(jacp1)
+        mj.mj_jacBody(mj_model, mj_data, jacp2, jacr2, id2)
+        with np.printoptions(suppress=True, linewidth=500):
+            print(jacp2)
 
 
 if __name__ == "__main__":
